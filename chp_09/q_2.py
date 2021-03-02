b19bd8b5a30ed2024e9d6b3f71bc6491ee3d498d
Ahmad Amin <ammyhunn@gmail.com>
2021-03-02 22:01:28 +0500

    dict: days count from file

---
+++ chp_09/q_2.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    fhand = open('demo.txt')
+    days = {}
+    for line in fhand:
+        if line.startswith('From'):
+            words = line.split()
+            if len(words) > 3:
+                days[words[2]] = days.get(words[2],0) +1
+    print(days)
---
+++ chp_09/remove_punctuation.py
@@ -0,0 +1,13 @@
+import string
+
+if __name__ == '__main__':
+    count = dict()
+    fhand = open('romeo.txt')
+    for line in fhand:
+        line = line.rstrip()
+        line = line.translate(line.maketrans('','',string.punctuation))
+        line = line.lower()
+        words = line.split()
+        for word in words:
+            count[word] = count.get(word,0) + 1
+    print(count)
