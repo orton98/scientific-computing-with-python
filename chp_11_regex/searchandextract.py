519a93902f77f33cb1d3264f9634db7c917d2aa5
Ahmad Amin <ammyhunn@gmail.com>
2021-03-27 07:01:01 +0500

    regular expression

--- chp_11_regex/searchandextract.py
+++ chp_11_regex/searchandextract.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     fhand = open('contact.txt')
     for line in fhand:
         line = line.rstrip()
-        x = re.findall("^X\S*: ([0-9.]+)", line)
+        # x = re.findall("^X\S*: ([0-9.]+)", line)
+        # x = re.findall('^Details:.*rev=([0-9.]+)', line)
+        x = re.findall('^From .* ([0-9][0-9]):', line)
         if len(x) > 0:
             print(x)
